4f5c9cb435eacaff07cc56e08643a7b6a3275bcb
George Adamson <george.adamson@softwareunity.com>
2013-08-07 15:29:28 +0100

    Improved readme/comments

--- src/breakpoints.js
+++ src/breakpoints.js
@@ -1,17 +1,17 @@
 
-// Rudimentary detection of display type using breakpoints defined in the CSS. Store the results in h3g.detect object.
+// Rudimentary detection of display type using breakpoints defined in the CSS. Store the results in global object variable.
 // The advantage of this technique is simply that the breakpoints don't have to be defined and duplicated in the JS too.
-// Eg: h3g.detect.phone === true
-// IMPORTANT: Must be used with detect-display.scss.
-// IMPORTANT: Must use with respond.js to use this Media Query technique in IE6-8 etc.
-// TODO: Perhaps this could be refactored as a Modernizr plugin?
+// Eg: window.breakpoints.phone === true
+// IMPORTANT: Must be used with breakpoints.scss.
+//            Use with respond.js to use this Media Query technique in IE6-8 etc.
+// By George Adamson - https://github.com/georgeadamson/breakpoints-js
+
+;(function( window, undefined ){
 
 /* jshint laxcomma:true, asi:true, debug:true, curly:false, camelcase:true, browser:true */
-/* global define, h3g, console */
+/* global define, console */
 
-;function( window, undefined ){
-
-  ;(function (factory) {
+  (function (factory) {
 
     // Register as an anonymous AMD module if relevant, otherwise assume oldskool browser globals:
     if (typeof define === "function" && define.amd)
@@ -21,15 +21,21 @@
 
   })(function( $ ) {
 
-    var moduleName      = 'breakpoints'
+    var moduleName     = 'breakpoints'
 
-      , stylesMissing   = 'missing-breakpoints-stylesheet'
-
-    // Define an object on which to define custom display-size properties: (Eg: breakpoints.phone = true & breakpoints.tablet = false)
-      , breakpoints     = window[moduleName] || ( window[moduleName] = {} )
+    // Define a global variable on which to define custom display-size properties: (Eg: breakpoints.phone = true & breakpoints.tablet = false)
+      , breakpoints    = window[moduleName] || ( window[moduleName] = {} )
 
     // Flag for tracking changes to the currently detected breakpoint:
-      , previous        = null
+      , previous       = null
+                       
+      , stylesMissing  = 'missing-breakpoints-stylesheet'
+
+      // Helper to read custom font from specified element:
+      , getFontOf      = function( elem ){
+        var font = $(elem).css('fontFamily')
+        return ( !font || /^(sans-)?serif/.test(font) ) ? '' : font
+      }
 
     // Helper to build a hash of breakpoint names. Used by detectBreakpoint(). The currently detected display will have a true value, the others false:
       , mapBreakpoints = function( names, current ){
@@ -39,9 +45,9 @@
         }
 
     // Detect iframe because we may need to respond to iframe-resize events, even on a device with fixed browser display size:
-      , inIframe        = window.top && window.top !== window
+      , inIframe       = window.top && window.top !== window
 
-      , resizeEvent     = ( 'onorientationchange' in window ) ? 'orientationchange' + ( inIframe ? 'resize' : '' ) : 'resize'
+      , resizeEvent    = ( 'onorientationchange' in window ) ? 'orientationchange' + ( inIframe ? 'resize' : '' ) : 'resize'
 
 
 
@@ -55,10 +61,10 @@
     })(function(){
 
       // Media Queries will set the name of the current breakpoint as a fake fontFamily on the hidden HEAD element. We simply read the corresponding values:
-      var $head    = $('HEAD:first')
-        , current  = $head.css('fontFamily') || stylesMissing             // Read currently applicable display type: (We rely on this to vary, depending on which mediaquery is being applied right now)
-        , names    = $head.children().css('fontFamily').split(/\s*,\s*/)  // Read list of all possible display variants defined in css so we can iterate through them: (See detect-display.scss for definition of these values)
-        , detected = mapBreakpoints( names, current )                     // Populate new breakpoints hash with a key for each of the breakpoint names defined in the css.
+      var head     = document.getElementsByTagName('head')[0]
+        , current  = getFontOf( head ) || stylesMissing               // Read currently applicable display type: (We rely on this to vary, depending on which mediaquery is being applied right now)
+        , names    = getFontOf( $(head).children() ).split(/\s*,\s*/) // Read list of all possible display variants defined in css so we can iterate through them: (See detect-display.scss for definition of these values)
+        , detected = mapBreakpoints( names, current )                 // Populate new breakpoints hash with a key for each of the breakpoint names defined in the css.
         , total    = names.length
         , i        = 0
         , name
